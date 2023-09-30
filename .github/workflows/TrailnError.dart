d3b1038c300748a10034702b08de0347f03876d1
Manasi Darade <104726010+ManasiDarade111@users.noreply.github.com>
2023-09-30 13:51:38 +0530

    Add files via upload

---
+++ .github/workflows/TrailnError.dart
@@ -0,0 +1,236 @@
+
+
+void main(){
+  /*ar x= "Core@web";
+  print(x);
+  x=20.5;
+  print(x);*/
+
+
+  /*int x =10;
+  print(x);
+  num y=20;
+  print(y);*/
+
+  /*var x=10;
+  var y=20.0;
+  var z= "Hello";
+  print(x.runtimeType);
+  print(y.runtimeType);
+  print(z.runtimeType);*/
+
+ /* var x=10;
+  x=19;
+  print(x);*/
+
+  /*var x= 10;
+  x=19.8;
+  print(x);// Error */
+
+  /*int x;
+  int y=10;
+  print(y);*/
+
+  /*for(int i=0; i<4; i++){
+    if (i==2){
+    continue;
+    }
+    print(i);
+  }*/
+
+ /* var sum=0;
+  for(var i=1 ; i<50; i++){
+    if(i%2 ==0 && i%3==0){
+      sum=sum+i;
+
+    }
+  }
+  print(sum);
+
+  int vl= 1;
+  for(int i=1;i<=8;i++){
+    if(i%2==0){
+      vl=vl*i;
+
+    }
+  }
+
+print(vl+16);
+
+
+num z= 30;
+for(num i=5;i<20;){
+  if(i%z==0){
+
+  }
+  i++; 
+}
+
+   int c=0;
+   int i=1;
+   while(i<=20){
+    if(i%2==0 && i% 4==0){
+      c++;
+
+    }
+    i++;
+
+   }
+
+print(c);
+
+
+
+int i=10;
+while (i>0){
+  if(i==8){
+break;
+
+  }
+  if(i==6){
+    i--;
+    continue;
+
+  }
+  print(i);
+  i--;
+}
+
+int x=1;
+while(x<=10){
+  if(++x ==10){
+    break;
+
+  }
+  print(x);
+  x++;
+
+}
+int num =10;
+while(num>0){
+  if(num%2 ==0){
+    print(num);
+
+  }
+  print("enf");
+}
+
+
+int num =5;
+while(num !=0){
+  print(num);
+  num~/=2;
+}
+
+
+int n=20;
+int s=0;
+while(n<=40)
+{
+  if(n%3==0){
+    s=s+n;
+
+  }
+  n++;
+
+}
+
+print(s);
+
+int num =12345;
+int sum =0;
+while(digit!=0){
+  int digit =num%10;
+  sum+=digit;
+  num=num~/10;
+
+
+  int n=6;
+  int x=1;
+  int i=1;
+
+  do{
+    x=x*i;
+    i=i+1;
+  
+
+  }while (i<=n);
+print(x);
+
+}
+
+
+int x=10;
+do{
+  print(x);
+  x-=3;
+
+}while(x>0 && x%2 ==0);
+
+
+
+do{
+  int x= 10;
+  print(x++);
+  x=x+2;
+}while(x ==50);
+
+} ErrorEvent
+
+
+
+int x=5;
+int sum=0;
+
+do{
+  sum=sum +x;
+  x++;
+  ++x;
+
+
+}while(x<=10);
+print(sum);
+
+
+int x=10;
+
+
+do{
+  print(x);
+
+ x= ++x +--x + ++x;
+print(x);
+
+}while(x==3);
+
+
+int x = 1;
+switch(x){
+
+case 1 :
+case 2 :
+case 3 :
+default:
+}
+print("End");*/
+
+
+int x = 10-9;
+switch(x){
+
+case 1 :
+print("one");
+case 2 :
+print("otwe");
+case 3 :
+print("three");
+
+}
+print("End");
+
+
+
+
+
+
+}
